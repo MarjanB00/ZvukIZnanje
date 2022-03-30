880482ff9d6b2e8a9c1cbf4225380294ef32b910
Marjan <marjanbilafer@gmail.com>
2022-03-30 14:29:45 +0200

    first commit

---
+++ src/main/java/com/project/zvukiznanje/ZvukIZnanjeApplication.java
@@ -0,0 +1,13 @@
+package com.project.zvukiznanje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZvukIZnanjeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZvukIZnanjeApplication.class, args);
+    }
+
+}
